c3ccbc458d6c6e03a82c8b294921de9db5c4b1c6
Gordon Mo <Gordon62105@gmail.com>
2026-05-16 21:53:24 -0400

    Removed url for deployment

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173" || 'https://hw4-web-dev.vercel.app/',
+  origin: "http://localhost:5173" || process.env.VITE_FRONTEND_URL,
   credentials: true,            
 }));
 
